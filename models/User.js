eef97c0c55592d5d4fc381cdc653199b04aae090
aPaulinhaCarlos <apaulinhacarlos@gmail.com>
2022-01-07 11:08:28 -0300

    ajusta validacoes no banco de dados

--- models/User.js
+++ models/User.js
@@ -1,49 +1,34 @@
+const validateDisplayName = {
+  len: {
+    args: [8],
+    msg: '"displayName" length must be at least 8 characters long',
+  },
+};
+
+const validateEmail = {
+  notEmpty: { msg: '"email" is required' },
+  isEmail: { msg: '"email" must be a valid email' },
+};
+
+const validatePassword = {
+  notEmpty: { msg: '"password" is required' },
+  len: {
+    args: [6, 6],
+    msg: '"password" length must be 6 characters long',
+  },
+};
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    displayName: {
-      type: DataTypes.STRING,
-      validate: {
-        min: {
-          args: 8,
-          msg: '"displayName" length must be at least 8 characters long',
-        },
-      },
-    },
-    email: {
-      type: DataTypes.STRING,
-      validate: {
-        notEmpty: {
-          msg: '"email" is required',
-        },
-        isEmail: {
-          msg: '"email" must be a valid email',
-        },
-      },
-    },
-    password: {
-      type: DataTypes.STRING,
-      validate: {
-        notEmpty: {
-          msg: '"password" is required',
-        },
-        len: {
-          args: [6, 6],
-          msg: '"password" length must be 6 characters long',
-        },
-      },
-    },
-    image: {
-      type: DataTypes.STRING,
-    },
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    displayName: { type: DataTypes.STRING, validate: validateDisplayName },
+    email: { type: DataTypes.STRING, validate: validateEmail },
+    password: { type: DataTypes.STRING, validate: validatePassword },
+    image: DataTypes.STRING,
   },
   {
     tableName: 'Users',
-    timestamps: true,
+    timestamps: false, // true obriga criar o createdAt e updateAt na migration
   });
   return User;
 };
